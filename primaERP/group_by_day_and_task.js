9d94403bcdc8c3bbdf8dfbbee973f40ac32edf18
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2017-09-03 14:23:59 +0300

    0.3.0: primaERP.GroupByDateAndTask - include round functions

--- primaERP/group_by_day_and_task.js
+++ primaERP/group_by_day_and_task.js
@@ -1,101 +1,129 @@
 "use strict";
 // ==UserScript==
-// @name         primaERP - group billable time by day and task
-// @namespace    http://tampermonkey.net/
-// @version      0.1.0
-// @description  primaERP - group billable time by day and task
-// @author       Alex Ulianytskyi <a.ulyanitsky@gmail.com>
-// @homepage     https://github.com/asux/userscripts/blob/master/primaERP/aggregate_by_day_and_time.js
-// @downloadURL  https://raw.githubusercontent.com/asux/userscripts/master/primaERP/aggregate_by_day_and_time.js
-// @match        https://*.primaerp.com/reports/*
-// @grant        none
-// @require      https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
-// @run-at       document-idle
+// @name           primaERP - group billable time by day and task
+// @namespace      http://tampermonkey.net/
+// @version        0.3.0
+// @description    primaERP - group billable time by day and task
+// @author         Alex Ulianytskyi <a.ulyanitsky@gmail.com>
+// @homepage       https://github.com/asux/userscripts/blob/master/primaERP/aggregate_by_day_and_time.js
+// @downloadURL    https://raw.githubusercontent.com/asux/userscripts/master/primaERP/aggregate_by_day_and_time.js
+// @match          https://*.primaerp.com/reports/*
+// @grant          none
+// @require        https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
+// @run-at         document-idle
 // ==/UserScript==
 // When run inside Fluid application
 if (typeof fluid === 'object') {
     fluid.include(fluid.resourcePath + 'scripts/waitForKeyElements.js');
 }
-function getCellText(cells, n) {
-    return $(cells[n]).text().trim();
-}
-function collectTimeRecords(root) {
-    var timeRecords = [];
-    root.find('tbody tr').each(function () {
-        var row = $(this);
-        if (row.hasClass('group-header')) {
-            return;
+var RoundTimerecords;
+(function (RoundTimerecords) {
+    function roundBy15Min(text) {
+        var matches = text.match(/(\d{2}):(\d{2})/);
+        if (Array.isArray(matches)) {
+            var hours = parseFloat(matches[1]);
+            var part = Math.ceil(parseFloat(matches[2]) / 15) / 4;
+            return hours + part;
         }
-        if (row.hasClass('record-description')) {
-            return;
-        }
-        if (row.hasClass('group-footer')) {
-            return;
-        }
-        var cells = row.find('td');
-        var timeRecord = {
-            date: getCellText(cells, 0),
-            time: getCellText(cells, 1),
-            user: getCellText(cells, 2),
-            client: getCellText(cells, 3),
-            project: getCellText(cells, 4),
-            task: getCellText(cells, 5),
-            activity: getCellText(cells, 6),
-            billableHours: parseFloat(getCellText(cells, 7)),
-            price: parseFloat(getCellText(cells, 8)),
-            billableUSD: parseFloat(getCellText(cells, 9))
-        };
-        timeRecords.push(timeRecord);
-    });
-    return timeRecords;
-}
-function groupByDateAndTask(collection) {
-    var result = [];
-    collection.forEach(function (item, index, array) {
-        var existed = result.find(function (element, index, array) {
-            return element.date == item.date && element.task == item.task;
+        return parseFloat(text);
+    }
+    RoundTimerecords.roundBy15Min = roundBy15Min;
+    function updateTimeRecords(root) {
+        root.find('td.right span.help').text(function () {
+            var text = $(this).text();
+            var rounded = roundBy15Min(text);
+            if (rounded !== null) {
+                return rounded.toFixed(2);
+            }
         });
-        if (existed) {
-            return;
-        }
-        ;
-        var grouped = array.filter(function (element, index, array) {
-            return element.date == item.date && element.task == item.task;
+    }
+    RoundTimerecords.updateTimeRecords = updateTimeRecords;
+})(RoundTimerecords || (RoundTimerecords = {}));
+var GroupByDateAndTask;
+(function (GroupByDateAndTask) {
+    function getCellText(cells, n) {
+        return $(cells[n]).text().trim();
+    }
+    function collectTimeRecords(root) {
+        var timeRecords = [];
+        root.find('tbody tr').each(function () {
+            var row = $(this);
+            if (row.hasClass('group-header')) {
+                return;
+            }
+            if (row.hasClass('record-description')) {
+                return;
+            }
+            if (row.hasClass('group-footer')) {
+                return;
+            }
+            var cells = row.find('td');
+            var timeRecord = {
+                date: getCellText(cells, 0),
+                time: getCellText(cells, 1),
+                user: getCellText(cells, 2),
+                client: getCellText(cells, 3),
+                project: getCellText(cells, 4),
+                task: getCellText(cells, 5),
+                activity: getCellText(cells, 6),
+                billableHours: RoundTimerecords.roundBy15Min(getCellText(cells, 7)),
+                price: parseFloat(getCellText(cells, 8)),
+                billableUSD: parseFloat(getCellText(cells, 9))
+            };
+            timeRecords.push(timeRecord);
         });
-        var sumBillableHours = grouped.reduce(function (acc, value, index) {
-            return acc + value.billableHours;
-        }, 0.0);
-        var resultItem = {
-            date: item.date,
-            task: item.task,
-            billableHours: sumBillableHours
-        };
-        console.log(resultItem.date + " | " + resultItem.task + " | " + resultItem.billableHours.toFixed(2));
-        result.push(resultItem);
-    });
-    return result;
-}
-function renderTableWithResults(container, results) {
-    var box = $("<div class=\"row space-after\">\n                  <div class=\"col-md-12\">\n                    <div class=\"box border space-after space-right\">\n                      <h2>Tasks summary</h2>\n                      <div class=\"boxcontent\"></div>\n                    </div>\n                  </div>\n                </div>");
-    var table = $('<table class="table table-condensed primaReportTable tasks-table"></table>');
-    var header = $("<thead>\n                    <tr>\n                    <th>Date</th>\n                    <th>Task</th>\n                    <th class=\"right\">Billable Hours</th>\n                    </tr>\n                  </thead>");
-    var tbody = $('<tbody></tbody>');
-    results.forEach(function (timeRecord, index, array) {
-        var row = $("<tr>\n                  <td>" + timeRecord.date + "</td>\n                  <td>" + timeRecord.task + "</td>\n                  <td class=\"right\">" + timeRecord.billableHours.toFixed(2) + "</td>\n                </tr>");
-        tbody.append(row);
-    });
-    table.append(header);
-    table.append(tbody);
-    $('.boxcontent', box).append(table);
-    container.append(box);
-}
-function collectAndGroupByDateAndTask(root) {
-    setTimeout(function () {
+        return timeRecords;
+    }
+    function groupByDateAndTask(collection) {
+        var result = [];
+        collection.forEach(function (item, index, array) {
+            var existed = result.find(function (element, index, array) {
+                return element.date == item.date && element.task == item.task;
+            });
+            if (existed) {
+                return;
+            }
+            ;
+            var grouped = array.filter(function (element, index, array) {
+                return element.date == item.date && element.task == item.task;
+            });
+            var sumBillableHours = grouped.reduce(function (acc, value, index) {
+                return acc + value.billableHours;
+            }, 0.0);
+            var resultItem = {
+                date: item.date,
+                task: item.task,
+                billableHours: sumBillableHours
+            };
+            console.log(resultItem.date + " | " + resultItem.task + " | " + resultItem.billableHours.toFixed(2));
+            result.push(resultItem);
+        });
+        return result;
+    }
+    function renderTableWithResults(container, results) {
+        var box = $("<div class=\"row space-after\">\n                        <div class=\"col-md-12\">\n                            <div class=\"box border space-after space-right\">\n                                <h2>Tasks summary</h2>\n                                <div class=\"boxcontent\"></div>\n                            </div>\n                        </div>\n                    </div>");
+        var table = $('<table class="table table-condensed primaReportTable tasks-table"></table>');
+        var header = $("<thead>\n                            <tr>\n                            <th>Date</th>\n                            <th>Task</th>\n                            <th class=\"right\">Billable Hours</th>\n                            </tr>\n                        </thead>");
+        var tbody = $('<tbody></tbody>');
+        results.forEach(function (timeRecord, index, array) {
+            var row = $("<tr>\n                            <td>" + timeRecord.date + "</td>\n                            <td>" + timeRecord.task + "</td>\n                            <td class=\"right\">" + timeRecord.billableHours.toFixed(2) + "</td>\n                        </tr>");
+            tbody.append(row);
+        });
+        table.append(header);
+        table.append(tbody);
+        $('.boxcontent', box).append(table);
+        container.append(box);
+    }
+    function collectAndGroupByDateAndTask(root) {
         var results = groupByDateAndTask(collectTimeRecords(root));
         renderTableWithResults(root.parents('.report'), results);
-    }, 1000);
-}
+    }
+    GroupByDateAndTask.collectAndGroupByDateAndTask = collectAndGroupByDateAndTask;
+})(GroupByDateAndTask || (GroupByDateAndTask = {}));
 if (typeof waitForKeyElements === 'function') {
     var tableSel = 'table.table-condensed.primaReportTable.summary-table';
-    waitForKeyElements(tableSel, collectAndGroupByDateAndTask);
+    waitForKeyElements(tableSel, function (root) {
+        RoundTimerecords.updateTimeRecords(root);
+        GroupByDateAndTask.collectAndGroupByDateAndTask(root);
+    });
 }
--- primaERP/group_by_day_and_task.ts
+++ primaERP/group_by_day_and_task.ts
@@ -1,128 +1,152 @@
 // ==UserScript==
-// @name         primaERP - group billable time by day and task
-// @namespace    http://tampermonkey.net/
-// @version      0.1.0
-// @description  primaERP - group billable time by day and task
-// @author       Alex Ulianytskyi <a.ulyanitsky@gmail.com>
-// @homepage     https://github.com/asux/userscripts/blob/master/primaERP/aggregate_by_day_and_time.js
-// @downloadURL  https://raw.githubusercontent.com/asux/userscripts/master/primaERP/aggregate_by_day_and_time.js
-// @match        https://*.primaerp.com/reports/*
-// @grant        none
-// @require      https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
-// @run-at       document-idle
+// @name           primaERP - group billable time by day and task
+// @namespace      http://tampermonkey.net/
+// @version        0.3.0
+// @description    primaERP - group billable time by day and task
+// @author         Alex Ulianytskyi <a.ulyanitsky@gmail.com>
+// @homepage       https://github.com/asux/userscripts/blob/master/primaERP/aggregate_by_day_and_time.js
+// @downloadURL    https://raw.githubusercontent.com/asux/userscripts/master/primaERP/aggregate_by_day_and_time.js
+// @match          https://*.primaerp.com/reports/*
+// @grant          none
+// @require        https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
+// @run-at         document-idle
 // ==/UserScript==
-
-interface TimeRecord {
-  date: string;
-  time?: string;
-  user?: string;
-  client?: string;
-  project?: string;
-  task: string;
-  activity?: string;
-  billableHours: number;
-  price?: number;
-  billableUSD?: number;
-}
 
 // When run inside Fluid application
 if (typeof fluid === 'object') {
-  fluid.include(fluid.resourcePath + 'scripts/waitForKeyElements.js');
+    fluid.include(fluid.resourcePath + 'scripts/waitForKeyElements.js');
 }
-
-function getCellText(cells: HTMLTableElement[], n: number) {
-  return $(cells[n]).text().trim();
-}
-
-function collectTimeRecords(root: JQuery): TimeRecord[] {
-  var timeRecords: TimeRecord[] = [];
-  root.find('tbody tr').each(function() {
-    let row: jQuery<HTMLTableRowElement> = $(this);
-    if (row.hasClass('group-header')) { return; }
-    if (row.hasClass('record-description')) { return; }
-    if (row.hasClass('group-footer')) { return; }
-    let cells = row.find('td');
-    let timeRecord: TimeRecord = {
-      date: getCellText(cells, 0),
-      time: getCellText(cells, 1),
-      user: getCellText(cells, 2),
-      client: getCellText(cells, 3),
-      project: getCellText(cells, 4),
-      task: getCellText(cells, 5),
-      activity: getCellText(cells, 6),
-      billableHours: parseFloat(getCellText(cells, 7)),
-      price:  parseFloat(getCellText(cells, 8)),
-      billableUSD:  parseFloat(getCellText(cells, 9))
+namespace RoundTimerecords {
+    export function roundBy15Min(text: string): number {
+        let matches = text.match(/(\d{2}):(\d{2})/);
+        if (Array.isArray(matches)) {
+            let hours: number = parseFloat(matches[1]);
+            let part: number =  Math.ceil(parseFloat(matches[2]) / 15) / 4;
+            return hours + part;
+        }
+        return parseFloat(text);
     }
-    timeRecords.push(timeRecord);
-  });
-  return timeRecords;
-}
 
-function groupByDateAndTask(collection: TimeRecord[]): TimeRecord[] {
-  var result: TimeRecord[] = [];
-  collection.forEach((item, index, array) => {
-    let existed = result.find((element, index, array) => {
-      return element.date == item.date && element.task == item.task;
-    });
-    if (existed) { return; };
-    let grouped: TimeRecord[] = array.filter((element, index, array) => {
-      return element.date == item.date && element.task == item.task;
-    });
-    let sumBillableHours: number = grouped.reduce((acc, value, index) => {
-      return acc + value.billableHours;
-    }, 0.0);
-    let resultItem: TimeRecord = {
-      date: item.date,
-      task: item.task,
-      billableHours: sumBillableHours
+    export function updateTimeRecords(root: JQuery): void {
+        root.find('td.right span.help').text(function() {
+            let text = $(this).text();
+            let rounded = roundBy15Min(text);
+            if (rounded !== null) {
+                return rounded.toFixed(2);
+            }
+        });
     }
-    console.log(`${resultItem.date} | ${resultItem.task} | ${resultItem.billableHours.toFixed(2)}`);
-    result.push(resultItem);
-  });
-  return result;
 }
+namespace GroupByDateAndTask {
+    interface TimeRecord {
+        date: string;
+        time?: string;
+        user?: string;
+        client?: string;
+        project?: string;
+        task: string;
+        activity?: string;
+        billableHours: number;
+        price?: number;
+        billableUSD?: number;
+    }
 
-function renderTableWithResults (container: JQuery<HTMLElement>, results: TimeRecord[]): void {
-  let box = $(`<div class="row space-after">
-                  <div class="col-md-12">
-                    <div class="box border space-after space-right">
-                      <h2>Tasks summary</h2>
-                      <div class="boxcontent"></div>
-                    </div>
-                  </div>
-                </div>`);
-  let table = $('<table class="table table-condensed primaReportTable tasks-table"></table>');
-  let header = $(`<thead>
-                    <tr>
-                    <th>Date</th>
-                    <th>Task</th>
-                    <th class="right">Billable Hours</th>
-                    </tr>
-                  </thead>`);
-  let tbody = $('<tbody></tbody>');
-  results.forEach((timeRecord, index, array) => {
-    let row = $(`<tr>
-                  <td>${timeRecord.date}</td>
-                  <td>${timeRecord.task}</td>
-                  <td class="right">${timeRecord.billableHours.toFixed(2)}</td>
-                </tr>`);
-    tbody.append(row);
-  });
-  table.append(header);
-  table.append(tbody);
-  $('.boxcontent', box).append(table);
-  container.append(box);
-}
+    function getCellText(cells: HTMLTableElement[], n: number) {
+        return $(cells[n]).text().trim();
+    }
 
-function collectAndGroupByDateAndTask(root: JQuery): void {
-  setTimeout(() => {
-    let results = groupByDateAndTask(collectTimeRecords(root));
-    renderTableWithResults(root.parents('.report'), results);
-  }, 1000);
+    function collectTimeRecords(root: JQuery): TimeRecord[] {
+        var timeRecords: TimeRecord[] = [];
+        root.find('tbody tr').each(function() {
+            let row: jQuery<HTMLTableRowElement> = $(this);
+            if (row.hasClass('group-header')) { return; }
+            if (row.hasClass('record-description')) { return; }
+            if (row.hasClass('group-footer')) { return; }
+            let cells = row.find('td');
+            let timeRecord: TimeRecord = {
+                date: getCellText(cells, 0),
+                time: getCellText(cells, 1),
+                user: getCellText(cells, 2),
+                client: getCellText(cells, 3),
+                project: getCellText(cells, 4),
+                task: getCellText(cells, 5),
+                activity: getCellText(cells, 6),
+                billableHours: RoundTimerecords.roundBy15Min(getCellText(cells, 7)),
+                price: parseFloat(getCellText(cells, 8)),
+                billableUSD: parseFloat(getCellText(cells, 9))
+            }
+            timeRecords.push(timeRecord);
+        });
+        return timeRecords;
+    }
+
+    function groupByDateAndTask(collection: TimeRecord[]): TimeRecord[] {
+        var result: TimeRecord[] = [];
+        collection.forEach((item, index, array) => {
+            let existed = result.find((element, index, array) => {
+                return element.date == item.date && element.task == item.task;
+            });
+            if (existed) { return; };
+            let grouped: TimeRecord[] = array.filter((element, index, array) => {
+                return element.date == item.date && element.task == item.task;
+            });
+            let sumBillableHours: number = grouped.reduce((acc, value, index) => {
+                return acc + value.billableHours;
+            }, 0.0);
+            let resultItem: TimeRecord = {
+                date: item.date,
+                task: item.task,
+                billableHours: sumBillableHours
+            }
+            console.log(`${resultItem.date} | ${resultItem.task} | ${resultItem.billableHours.toFixed(2)}`);
+            result.push(resultItem);
+        });
+        return result;
+    }
+
+    function renderTableWithResults (container: JQuery<HTMLElement>, results: TimeRecord[]): void {
+        let box = $(`<div class="row space-after">
+                        <div class="col-md-12">
+                            <div class="box border space-after space-right">
+                                <h2>Tasks summary</h2>
+                                <div class="boxcontent"></div>
+                            </div>
+                        </div>
+                    </div>`);
+        let table = $('<table class="table table-condensed primaReportTable tasks-table"></table>');
+        let header = $(`<thead>
+                            <tr>
+                            <th>Date</th>
+                            <th>Task</th>
+                            <th class="right">Billable Hours</th>
+                            </tr>
+                        </thead>`);
+        let tbody = $('<tbody></tbody>');
+        results.forEach((timeRecord, index, array) => {
+            let row = $(`<tr>
+                            <td>${timeRecord.date}</td>
+                            <td>${timeRecord.task}</td>
+                            <td class="right">${timeRecord.billableHours.toFixed(2)}</td>
+                        </tr>`);
+            tbody.append(row);
+        });
+        table.append(header);
+        table.append(tbody);
+        $('.boxcontent', box).append(table);
+        container.append(box);
+    }
+
+    export function collectAndGroupByDateAndTask(root: JQuery): void {
+        let results = groupByDateAndTask(collectTimeRecords(root));
+        renderTableWithResults(root.parents('.report'), results);
+    }
+
 }
 
 if (typeof waitForKeyElements === 'function') {
-  let tableSel = 'table.table-condensed.primaReportTable.summary-table';
-  waitForKeyElements(tableSel, collectAndGroupByDateAndTask);
+    let tableSel = 'table.table-condensed.primaReportTable.summary-table';
+    waitForKeyElements(tableSel, (root) => {
+        RoundTimerecords.updateTimeRecords(root);
+        GroupByDateAndTask.collectAndGroupByDateAndTask(root);
+    });
 }
